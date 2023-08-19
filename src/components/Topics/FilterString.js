a3ace5cdd4382d65fa826be0d273ae2e09353351
Jessica Swenson <jessicamswens@gmail.com>
2023-08-19 10:58:15 -0600

    added filter string

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FilterString = () => {
+    const [filteredArray, setFilteredArray] = useState([]);
+    const [userInput, setUserInput] = useState('');
+    const [unfilteredArray, setUnfilteredArray] = useState(['James', 'Jessica', 'Melody', 'Tyler', 'Blake', 'Jennifer', 'Mark', 'Maddy']);
+
+    const clickHandler = () => {
+        const filter = []
+        unfilteredArray.map(el => {
+            if (el.includes(userInput)) {
+                filter.push(el)
+            }
+        })
+        setFilteredArray(filter)
+    }
+
+
     return (
-        <div>FilterString</div>
+        <div className='puzzleBox filterStringPB'>
+            <h4>Filter String</h4>
+            <span className='puzzleText'>Names: {JSON.stringify(unfilteredArray)}</span>
+            <input onChange={e => setUserInput(e.target.value)} className='inputLine' />
+            <button onClick={clickHandler} className='confirmationButton'>Filter</button>
+            <span className='resultsBox filterStringRB'>Filtered Names: {JSON.stringify(filteredArray)}</span>
+        </div>
     )
 }
 
