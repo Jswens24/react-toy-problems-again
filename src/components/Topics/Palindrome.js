93953327c991a2028e20f6833b11668ea71cf0f9
Jessica Swenson <jessicamswens@gmail.com>
2023-08-19 13:08:48 -0600

    finished palindrome

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Palindrome = () => {
+    const [userInput, setUserInput] = useState('');
+    const [palindrome, setPalindrome] = useState('');
+
+    const onClickHandler = () => {
+        if (userInput.split('').reverse().join('') === userInput) {
+            setPalindrome('True')
+        } else {
+            setPalindrome("False")
+        }
+        return palindrome
+    }
+
     return (
-        <div>Palindrome</div>
+        <div className='puzzleBox filterStringPB'>
+            <h4>Palindrome</h4>
+            <input onChange={e => setUserInput(e.target.value)} className='inputLine' />
+            <button onClick={onClickHandler} className='confirmationButton'>Check</button>
+            <span className='resultsBox'>Palindrome: {palindrome}</span>
+        </div>
     )
 }
 
